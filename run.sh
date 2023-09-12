dd5ee45a190964b76e3d349d1347da007aa3df0a
= <ornithopter944@gmail.com>
2023-09-12 20:54:30 +0200

    Small changes to run.sh and runc.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 # Run without compiling.
+# Remember to chmod +x this and run it without sudo.
 sudo qemu-system-x86_64 -drive format=raw,file=target/x86_64-rust_os/debug/bootimage-rust_os.bin # Run in qemu
