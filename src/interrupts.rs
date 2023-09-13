cfa1f88df03c21bf42d6050c253ed2bada6f4364
= <ornithopter944@gmail.com>
2023-09-13 21:01:33 +0200

    Added double fault handling

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -12,6 +12,7 @@ lazy_static! {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
         idt.page_fault.set_handler_fn(page_fault_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     };
 }
@@ -30,4 +31,9 @@ extern "x86-interrupt" fn page_fault_handler(
 {
     println!("EXCEPTION: PAGE FAULT:{:?}\n{:#?}", page_fault_code, stack_frame);
     hlt();
+}
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame, _error_code: u64) -> !
+{
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
