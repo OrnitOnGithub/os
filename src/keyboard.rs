ae4616cde772551031f88138200319a3074f20f7
OrnitOnGithub <lea944.lastname@gmail.com>
2023-10-03 21:34:24 +0200

    Update experimental

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,3 +1,23 @@
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+
+
+pub struct KeyboardState {
+    shift:    bool,
+    capslock: bool,
+    ctrl:     bool,
+}
+// Create a static mutex for the keyboard state.
+lazy_static! {
+    static ref KEYBOARD_STATE: Mutex<KeyboardState> = Mutex::new(KeyboardState {
+        shift:    false,
+        capslock: false,
+        ctrl:     false,
+    });
+}
+/// This function should:
+/// - Do something
+/// - Somehow inform the API / AKI
 pub fn handle_keyboard(scancode: u8) {
-    
+
 }
