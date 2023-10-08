c5f522e8e51da60318070cd3d0e2866c27a85c20
OrnitOnGithub <lea944.lastname@gmail.com>
2023-10-08 18:02:03 +0200

    Updated run.sh and runc.sh for the nvme drive

--- runc.sh
+++ runc.sh
@@ -2,4 +2,4 @@
 # Remember to chmod +x this and run it without sudo.
 cargo bootimage # Compile and turn into bootimage using cargo crate "bootimage"
 echo ""
-sudo qemu-system-x86_64 -drive format=raw,file=target/x86_64-rust_os/debug/bootimage-rust_os.bin # Run in qemu
+sudo qemu-system-x86_64 -drive format=raw,file=target/x86_64-rust_os/debug/bootimage-rust_os.bin -drive file=emulated/nvm.img,if=none,id=nvm -device nvme,serial=deadbeef,drive=nvm
