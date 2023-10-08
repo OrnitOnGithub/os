2721137c3dc2158d8955aa324c221413f25a77e2
OrnitOnGithub <lea944.lastname@gmail.com>
2023-10-08 18:14:03 +0200

    fixed run.sh and runc.sh again

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 # Run without compiling.
 # Remember to chmod +x this and run it without sudo.
+
+# Run in QEMU
 sudo qemu-system-x86_64 -drive format=raw,file=target/x86_64-rust_os/debug/bootimage-rust_os.bin -drive file=emulated/nvm.img,if=none,id=nvm -device nvme,serial=deadbeef,drive=nvm
