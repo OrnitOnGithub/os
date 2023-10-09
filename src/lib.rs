9b7a8f21fa03a9507d1d92341c3af78b4aaea5e3
OrnitOnGithub <lea944.lastname@gmail.com>
2023-10-09 18:32:25 +0200

    Added hlt_loop() to idle more efficiently using x86 instruction 'hlt'

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 pub mod interrupts;
 pub mod vga_buffer;
 pub mod gdt;
+pub mod keyboard;
 
 // Initiate all the initiatables. Called once at system startup.
 pub fn init() {
@@ -10,4 +11,11 @@ pub fn init() {
     interrupts::init_idt();
     unsafe { interrupts::PICS.lock().initialize() };
     x86_64::instructions::interrupts::enable();
+}
+// Replaces the loop {} After the OS initialisation.
+// Much more resource efficiet way for the Kernel to do nothing
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
